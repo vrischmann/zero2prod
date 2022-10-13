cabc3d909471327f98d80cc703156cd965c49540
Vincent Rischmann <vincent@rischmann.fr>
2022-10-13 23:34:40 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
