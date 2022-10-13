d959a37b7c0dba5f2cc88243221a1a8f04916b3a
Vincent Rischmann <vincent@rischmann.fr>
2022-10-13 23:28:41 +0200

    update

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+use std::io;
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run() -> Result<Server, io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
+
+    Ok(server)
+}
