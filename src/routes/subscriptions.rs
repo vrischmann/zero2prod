dd7037e9e6d136357fddbc6a195075dffbbd10c1
Vincent Rischmann <vincent@rischmann.fr>
2022-10-16 22:28:14 +0200

    update

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,6 +11,13 @@ pub struct FormData {
 }
 
 pub async fn subscribe(pool: web::Data<PgPool>, form: web::Form<FormData>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!("Adding a new subscriber", %request_id, subscriber_email=%form.email, subscriber_name=%form.name);
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions(id, email, name, subscribed_at)
@@ -20,11 +28,22 @@ pub async fn subscribe(pool: web::Data<PgPool>, form: web::Form<FormData>) -> im
         Utc::now(),
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id={} New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok()
+        }
         Err(err) => {
-            println!("unable to insert into subscriptions, err: {}", err);
+            tracing::error!(
+                "request_id={} unable to insert into subscriptions, err: {:?}",
+                request_id,
+                err
+            );
             HttpResponse::InternalServerError()
         }
     }
