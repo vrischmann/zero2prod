aaf45e7a71da80526c9838d334986663c42190b6
Vincent Rischmann <vincent@rischmann.fr>
2022-10-16 23:24:29 +0200

    update

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,16 @@
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::io;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, io::Error> {
     let pool = web::Data::new(pool);
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(crate::routes::health_check))
             .route("/subscriptions", web::post().to(crate::routes::subscribe))
             .app_data(pool.clone())
