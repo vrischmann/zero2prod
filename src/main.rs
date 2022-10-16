005e1e330609b3cd2deae07677c8029516d75c84
Vincent Rischmann <vincent@rischmann.fr>
2022-10-16 22:31:50 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
@@ -10,6 +11,8 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
     let subscriber = Registry::default()
@@ -17,6 +20,8 @@ async fn main() -> io::Result<()> {
         .with(JsonStorageLayer)
         .with(formatting_layer);
     set_global_default(subscriber).expect("Failed to set subscriber");
+
+    //
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let pool = PgPool::connect(&configuration.database.connection_string())
