a0daefea930d99ce0ab9893a48ddd2cb45348279
Vincent Rischmann <vincent@rischmann.fr>
2022-10-16 23:49:20 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::io;
 use std::net::TcpListener;
+use std::time::Duration;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry;
@@ -17,7 +18,9 @@ async fn main() -> io::Result<()> {
 
     tracing::info!(application_host=%configuration.application.host, "got configuration");
 
-    let pool = PgPool::connect(configuration.database.connection_string().expose_secret())
+    let pool = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(2))
+        .connect(configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to PostgreSQL");
 
