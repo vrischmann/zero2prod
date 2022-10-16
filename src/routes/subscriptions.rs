f7b8f7a0a96ef9c5b8658e4a3d35f1455926ab81
Vincent Rischmann <vincent@rischmann.fr>
2022-10-16 18:23:15 +0200

    make clippy happy

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -7,5 +7,9 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    let inner = form.into_inner();
+    let _ = inner.name;
+    let _ = inner.email;
+
     HttpResponse::Ok()
 }
