21ecd957cfeef2980f6b1b74cb58008120324272
Vincent Rischmann <vincent@rischmann.fr>
2022-10-18 00:48:10 +0200

    add a script and dockerfile to build the release deb package

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM rust:1.64
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y lld clang
+RUN cargo install cargo-deb
+
+COPY Cargo.toml Cargo.toml
+COPY Cargo.lock Cargo.lock
+COPY sqlx-data.json sqlx-data.json
+COPY src src
+
+ENV RUSTFLAGS="-C target-cpu=ivybridge"
+
+RUN cargo deb --no-strip
+RUN ls -lh /app/target
---
+++ build-deb.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+
+set -l _version "0.1.0"
+
+# Aborts the installer and displays an error.
+function abort -a message
+  if test -n "$message"
+    printf "%saborted: $message%s\n" (set_color -o red 2> /dev/null) (set_color normal 2> /dev/null) >&2
+  else
+    printf "%saborted%s\n" (set_color -o red 2> /dev/null) (set_color normal 2> /dev/null) >&2
+  end
+
+  exit 1
+end
+
+
+docker buildx build -t zero2prod .
+  or die "failed to build the docker builder image"
+
+set -l _id (docker create zero2prod)
+
+docker cp "$_id:/app/target/debian/zero2prod_"$_version"_amd64.deb" .
