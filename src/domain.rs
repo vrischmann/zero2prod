62066faf5a8ac177d7f56a134e1a38e3e6520e7d
Vincent Rischmann <vincent@rischmann.fr>
2022-10-25 23:24:26 +0200

    add SubscriberEmail type, more tests

--- src/domain.rs
+++ src/domain.rs
@@ -1,7 +1,8 @@
 use unicode_segmentation::UnicodeSegmentation;
+use validator::validate_email;
 
 pub struct NewSubscriber {
-    pub email: String,
+    pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
 
@@ -30,10 +31,31 @@ impl AsRef<str> for SubscriberName {
     }
 }
 
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        if validate_email(&s) {
+            Ok(SubscriberEmail(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use claim::{assert_err, assert_ok};
+
+    // Name tests
 
     #[test]
     fn a_256_grapheme_long_name_is_valid() {
@@ -54,7 +76,7 @@ mod tests {
     }
 
     #[test]
-    fn empty_string_is_rejected() {
+    fn empty_name_is_rejected() {
         let name = "".to_string();
         assert_err!(SubscriberName::parse(name));
     }
@@ -71,5 +93,25 @@ mod tests {
     fn a_valid_name_is_parsed_successfully() {
         let name = "Ursula Le Guin".to_string();
         assert_ok!(SubscriberName::parse(name));
+    }
+
+    // Email tests
+
+    #[test]
+    fn empty_email_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
     }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -24,10 +24,12 @@ pub async fn subscribe(pool: web::Data<PgPool>, form: web::Form<FormData>) -> Ht
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
     };
+
+    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -48,7 +50,7 @@ async fn insert_subscriber(
         INSERT INTO subscriptions(id, email, name, subscribed_at)
         VALUES($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now(),
     )
