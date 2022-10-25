55b7eebc4753b823655da131e4e9fe1af42cd0e2
Vincent Rischmann <vincent@rischmann.fr>
2022-10-25 23:37:17 +0200

    add some property based testing

--- src/domain.rs
+++ src/domain.rs
@@ -54,6 +54,7 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::*;
     use claim::{assert_err, assert_ok};
+    use fake::Fake;
 
     // Name tests
 
@@ -113,5 +114,20 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = fake::faker::internet::en::SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
