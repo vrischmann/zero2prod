c77cd89a934494cc571f658a51aa727451e7cc10
Vincent Rischmann <vincent@rischmann.fr>
2022-10-29 20:40:02 +0200

    tests/api: add a complete test for the subscription

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,4 +1,10 @@
-use crate::helpers::spawn_app;
+use crate::helpers::{spawn_app, SubscriptionBody, UrlEncodedBody};
+use fake::faker::internet::en::SafeEmail;
+use fake::faker::name::en::Name;
+use fake::Fake;
+use reqwest::Url;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn confirmations_without_token_are_rejected_with_a_400() {
@@ -9,4 +15,46 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
         .unwrap();
 
     assert_eq!(response.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
+    let app = spawn_app().await;
+
+    let body = SubscriptionBody {
+        name: Name().fake(),
+        email: SafeEmail().fake(),
+    };
+
+    Mock::given(path("/emails"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    let _ = app.post_subscriptions(body.encode()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| -> String {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+
+        assert_eq!(links.len(), 1, "expected 1 link, got {}", links.len());
+
+        links[0].as_str().to_owned()
+    };
+
+    let raw_confirmation_link = &get_link(body["html"].as_str().unwrap());
+    let mut confirmation_link = Url::parse(raw_confirmation_link).unwrap();
+    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+    confirmation_link.set_port(Some(app.port)).unwrap();
+
+    //
+
+    let response = reqwest::get(confirmation_link).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 200);
 }
