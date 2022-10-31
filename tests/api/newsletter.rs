dcb0bdafbee67a6e7ef849400c3ffe7a58336521
Vincent Rischmann <vincent@rischmann.fr>
2022-10-31 12:05:35 +0100

    tests/api/newsletter: add test for confirmed subscribers

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,4 @@
+use crate::helpers::ConfirmationLinks;
 use crate::helpers::{spawn_app, SubscriptionBody, TestApp, UrlEncodedBody};
 use fake::faker::internet::en::SafeEmail;
 use fake::faker::name::en::Name;
@@ -38,7 +39,37 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers(pool: sqlx::Pg
     assert_eq!(response.status().as_u16(), 200);
 }
 
-async fn create_unconfirmed_subscriber(app: &TestApp) {
+#[sqlx::test]
+async fn newsletters_are_delivered_to_unconfirmed_subscribers(pool: sqlx::PgPool) {
+    let app = spawn_app(pool).await;
+
+    create_confirmed_subscriber(&app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>"
+        },
+    });
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = SubscriptionBody {
         name: Name().fake(),
         email: SafeEmail().fake(),
@@ -54,6 +85,26 @@ async fn create_unconfirmed_subscriber(app: &TestApp) {
 
     app.post_subscriptions(body.encode())
         .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    app.get_confirmation_links(email_request)
+}
+
+async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_links = create_unconfirmed_subscriber(app).await;
+
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
         .error_for_status()
         .unwrap();
 }
