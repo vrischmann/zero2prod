89f07256403a0070fdfe4174ebbf84ebfb54cf51
Vincent Rischmann <vincent@rischmann.fr>
2022-11-01 00:53:42 +0100

    routes/newsletters: spawn a new task for verifying the password

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -10,7 +10,7 @@ use anyhow::{anyhow, Context};
 use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use secrecy::{ExposeSecret, Secret};
 use std::fmt;
-use tracing::{error, info_span};
+use tracing::error;
 use uuid::Uuid;
 
 #[derive(thiserror::Error)]
@@ -142,21 +142,39 @@ async fn validate_credentials(
 
     //
 
+    //
+
+    let verify_result = tokio::task::spawn_blocking(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
+    .await
+    .context("Failed to spawn blocking task")
+    .map_err(PublishError::Unexpected)?;
+
+    verify_result?;
+
+    Ok(user_id)
+}
+
+#[tracing::instrument(
+    name = "Verify password hash",
+    skip(expected_password_hash, password_candidate)
+)]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_candidate: Secret<String>,
+) -> Result<(), PublishError> {
     let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
         .context("Failed to parse hash in PHC string format")
         .map_err(PublishError::Unexpected)?;
 
-    info_span!("Verify password hash")
-        .in_scope(|| {
-            Argon2::default().verify_password(
-                credentials.password.expose_secret().as_bytes(),
-                &expected_password_hash,
-            )
-        })
+    Argon2::default()
+        .verify_password(
+            password_candidate.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
         .context("Invalid password")
-        .map_err(PublishError::Auth)?;
-
-    Ok(user_id)
+        .map_err(PublishError::Auth)
 }
 
 #[tracing::instrument(name = "Get stored credentials", skip(pool))]
