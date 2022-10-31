eba8194944bcac3718a42be2ee706daff53c850c
Vincent Rischmann <vincent@rischmann.fr>
2022-10-31 11:13:13 +0100

    tests/api: add integration tests for the newsletters endpoint

---
+++ tests/api/newsletter.rs
@@ -0,0 +1,59 @@
+use crate::helpers::{spawn_app, SubscriptionBody, TestApp, UrlEncodedBody};
+use fake::faker::internet::en::SafeEmail;
+use fake::faker::name::en::Name;
+use fake::Fake;
+use serde_json::json;
+use wiremock::matchers::{any, method, path};
+use wiremock::{Mock, ResponseTemplate};
+
+#[sqlx::test]
+async fn newsletters_are_not_delivered_to_unconfirmed_subscribers(pool: sqlx::PgPool) {
+    let app = spawn_app(pool).await;
+
+    create_unconfirmed_subscriber(&app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&app.email_server)
+        .await;
+
+    //
+
+    let newsletter_request_body = json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>"
+        },
+    });
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+async fn create_unconfirmed_subscriber(app: &TestApp) {
+    let body = SubscriptionBody {
+        name: Name().fake(),
+        email: SafeEmail().fake(),
+    };
+
+    let _mock_guard = Mock::given(path("/emails"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber")
+        .expect(1)
+        .mount_as_scoped(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.encode())
+        .await
+        .error_for_status()
+        .unwrap();
+}
