9c8c8ec589afd8b66d76db709ad237147f080996
Vincent Rischmann <vincent@rischmann.fr>
2022-10-31 20:37:40 +0100

    routes/newsletters: require basic credentials

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,15 +1,20 @@
 use crate::domain::SubscriberEmail;
 use crate::routes::error_chain_fmt;
 use crate::tem;
+use actix_web::http::header;
+use actix_web::http::header::{HeaderMap, HeaderValue};
 use actix_web::http::StatusCode;
 use actix_web::web;
-use actix_web::HttpResponse;
+use actix_web::{HttpRequest, HttpResponse};
 use anyhow::{anyhow, Context};
+use secrecy::Secret;
 use std::fmt;
 use tracing::error;
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed")]
+    Auth(#[source] anyhow::Error),
     #[error(transparent)]
     Unexpected(#[from] anyhow::Error),
 }
@@ -21,9 +26,19 @@ impl fmt::Debug for PublishError {
 }
 
 impl actix_web::ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
+    fn error_response(&self) -> HttpResponse {
         match self {
-            PublishError::Unexpected(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::Auth(_) => {
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                response
+                    .headers_mut()
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+
+                response
+            }
+            Self::Unexpected(_) => HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR),
         }
     }
 }
@@ -43,8 +58,11 @@ pub struct Content {
 pub async fn publish_newsletter(
     pool: web::Data<sqlx::PgPool>,
     email_client: web::Data<tem::Client>,
+    request: HttpRequest,
     body: web::Json<BodyData>,
 ) -> Result<HttpResponse, PublishError> {
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::Auth)?;
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -96,4 +114,45 @@ async fn get_confirmed_subscribers(
     .collect();
 
     Ok(result)
+}
+
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // Get the header value, decode as base64
+
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?;
+    let header_value_string = header_value
+        .to_str()
+        .context("The 'Authorization' header was not a valid ASCII string")?;
+
+    let encoded_segment = header_value_string
+        .strip_prefix("Basic ")
+        .context("The 'Authorization' header scheme was not 'Basic'")?;
+
+    let decoded_bytes = base64::decode_config(encoded_segment, base64::STANDARD)
+        .context("Failed to base64 decode the 'Basic' credentials")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credentials string is not valid UTF-8")?;
+
+    //
+
+    let mut credentials_iter = decoded_credentials.splitn(2, ':');
+
+    let username = credentials_iter
+        .next()
+        .ok_or_else(|| anyhow!("A username must be provided in 'Basic' auth"))?;
+    let password = credentials_iter
+        .next()
+        .ok_or_else(|| anyhow!("A password must be provided in 'Basic' auth"))?;
+
+    Ok(Credentials {
+        username: username.to_string(),
+        password: Secret::new(password.to_string()),
+    })
 }
