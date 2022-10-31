0bca472fcd25e01df2d63e3870738658bca2ac20
Vincent Rischmann <vincent@rischmann.fr>
2022-10-31 14:38:14 +0100

    routes: move error_chain_fmt to the routes root

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,20 @@
+pub use health_check::*;
+pub use newsletters::*;
+use std::fmt;
+pub use subscriptions::*;
+pub use subscriptions_confirm::*;
+
 pub mod health_check;
 pub mod newsletters;
 pub mod subscriptions;
 pub mod subscriptions_confirm;
 
-pub use health_check::*;
-pub use newsletters::*;
-pub use subscriptions::*;
-pub use subscriptions_confirm::*;
+pub fn error_chain_fmt(err: &impl std::error::Error, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    writeln!(f, "{}\n", err)?;
+    let mut current = err.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
