4a12044da457c151149ec6e866529b06faa87afb
Vincent Rischmann <vincent@rischmann.fr>
2022-11-01 11:58:41 +0100

    routes: add home

---
+++ src/routes/home.rs
@@ -0,0 +1,15 @@
+use actix_web::HttpResponse;
+use actix_web::http::header::ContentType;
+use askama::Template;
+
+#[derive(askama::Template)]
+#[template(path = "home.html")]
+pub struct HomeTemplate {}
+
+pub async fn home() -> HttpResponse {
+    let tpl = HomeTemplate {};
+
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(tpl.render().unwrap())
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,12 @@
 pub use health_check::*;
+pub use home::*;
 pub use newsletters::*;
 use std::fmt;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
 
 mod health_check;
+mod home;
 mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
