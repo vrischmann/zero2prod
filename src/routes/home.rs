d8417966b3029d0329b40bd6d4b8f1892080318d
Vincent Rischmann <vincent@rischmann.fr>
2022-11-05 23:12:59 +0100

    routes/home: set the flash messages field in the template

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -4,10 +4,16 @@ use askama::Template;
 
 #[derive(askama::Template)]
 #[template(path = "home.html.j2")]
-pub struct HomeTemplate {}
+pub struct HomeTemplate {
+    error_messages: Vec<String>,
+    info_messages: Vec<String>,
+}
 
 pub async fn home() -> HttpResponse {
-    let tpl = HomeTemplate {};
+    let tpl = HomeTemplate {
+        error_messages: Vec::new(),
+        info_messages: Vec::new(),
+    };
 
     HttpResponse::Ok()
         .content_type(ContentType::html())
