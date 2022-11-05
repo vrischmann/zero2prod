59573bcb1e1721b21669652424115c34d994b296
Vincent Rischmann <vincent@rischmann.fr>
2022-11-05 21:39:19 +0100

    tests/api: add the assert_is_redirect_to helper

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -191,6 +191,11 @@ impl TestUser {
     }
 }
 
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), location);
+}
+
 #[derive(serde::Serialize)]
 pub struct SubscriptionBody {
     pub name: String,
