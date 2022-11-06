f93bcfd8d271df321ff5ab1ddf071474fca406b8
Vincent Rischmann <vincent@rischmann.fr>
2022-11-06 23:17:19 +0100

    routes/admin_dashboard: implement the handler with a template

--- src/routes/admin_dashboard.rs
+++ src/routes/admin_dashboard.rs
@@ -1,5 +1,65 @@
+use actix_session::Session;
+use actix_web::http::header::ContentType;
+use actix_web::web;
 use actix_web::HttpResponse;
+use anyhow::Context;
+use askama::Template;
+use std::fmt;
+use uuid::Uuid;
 
-pub async fn admin_dashboard() -> HttpResponse {
-    HttpResponse::Ok().finish()
+fn internal_server_error<T>(err: T) -> actix_web::Error
+where
+    T: fmt::Debug + fmt::Display + 'static,
+{
+    actix_web::error::ErrorInternalServerError(err)
+}
+
+#[derive(askama::Template)]
+#[template(path = "admin_dashboard.html.j2")]
+pub struct LoginTemplate {
+    error_messages: Vec<String>,
+    info_messages: Vec<String>,
+    username: String,
+}
+
+pub async fn admin_dashboard(
+    pool: web::Data<sqlx::PgPool>,
+    session: Session,
+) -> Result<HttpResponse, actix_web::Error> {
+    let get_user_id_result = session
+        .get::<Uuid>("user_id")
+        .map_err(internal_server_error)?;
+
+    let username = match get_user_id_result {
+        Some(user_id) => get_username(&pool, user_id)
+            .await
+            .map_err(internal_server_error)?,
+        None => todo!(),
+    };
+
+    let tpl = LoginTemplate {
+        error_messages: Vec::new(),
+        info_messages: Vec::new(),
+        username,
+    };
+
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(tpl.render().unwrap()))
+}
+
+#[tracing::instrument(name = "Get username", skip(pool))]
+async fn get_username(pool: &sqlx::PgPool, user_id: Uuid) -> Result<String, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT username FROM users
+        WHERE user_id = $1
+        "#,
+        user_id
+    )
+    .fetch_one(pool)
+    .await
+    .context("Failed to perform query to retrieve the username")?;
+
+    Ok(row.username)
 }
