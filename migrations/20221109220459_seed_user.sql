4aa59224ba1cf089b506b51c970836358227b094
Vincent Rischmann <vincent@rischmann.fr>
2022-11-09 23:12:10 +0100

    migrations: add a seed user

---
+++ migrations/20221109220459_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(user_id, username, password_hash)
+VALUES(
+  '1ea5d586-83fb-42d0-b0b1-f99ccbc8dd79',
+  'admin',
+  '$argon2id$v=19$m=15000,t=2,p=1$kpB0Xtxi6W0qZlvODmTeFg$PxBy2Wi0tHG/Nqvs2Uv8U2O8tQiO6DXNuHoyoeGZE3w'
+);
