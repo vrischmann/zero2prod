a3a0b01d4eba6be1aa4b9ba279f6cf0ddf1e5057
Vincent Rischmann <vincent@rischmann.fr>
2022-11-20 18:42:58 +0100

    authentication/middleware: store the user id in the request extension data

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -4,8 +4,27 @@ use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::error::InternalError;
 use actix_web::FromRequest;
+use actix_web::HttpMessage;
 use actix_web_lab::middleware::Next;
 use anyhow::anyhow;
+use std::ops::Deref;
+use uuid::Uuid;
+
+#[derive(Copy, Clone, Debug)]
+pub struct UserId(Uuid);
+
+impl std::fmt::Display for UserId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Deref for UserId {
+    type Target = Uuid;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 pub async fn reject_anonymous_users(
     mut req: ServiceRequest,
@@ -19,7 +38,10 @@ pub async fn reject_anonymous_users(
 
     let user_id_result = session.get_user_id().map_err(to_internal_server_error)?;
     match user_id_result {
-        Some(_) => next.call(req).await,
+        Some(user_id) => {
+            req.extensions_mut().insert(UserId(user_id));
+            next.call(req).await
+        }
         None => {
             let response = see_other("/login");
             let err = anyhow!("The user has not logged in");
