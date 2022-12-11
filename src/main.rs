b815e7f63dc017971b36f315ecb024b1c2f81fd4
Vincent Rischmann <vincent@rischmann.fr>
2022-12-11 18:25:40 +0100

    main: start the issue delivery worker

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fmt::{Debug, Display};
+use tracing::{error, info};
 use zero2prod::configuration::get_configuration;
+use zero2prod::issue_delivery_worker;
 use zero2prod::startup::Application;
 use zero2prod::telemetry;
 
@@ -17,8 +20,44 @@ async fn main() -> anyhow::Result<()> {
         "got configuration",
     );
 
-    let app = Application::build(configuration).await?;
-    app.run_until_stopped().await?;
+    let app = Application::build(configuration.clone()).await?;
+
+    let app_task = tokio::spawn(app.run_until_stopped());
+    let worker_task = tokio::spawn(issue_delivery_worker::run_worker_until_stopped(
+        configuration,
+    ));
+
+    tokio::select! {
+        outcome = app_task => report_exit("API", outcome),
+        outcome = worker_task => report_exit("Issue delivery worker", outcome),
+    };
 
     Ok(())
+}
+
+fn report_exit(
+    task_name: &str,
+    outcome: Result<Result<(), impl Debug + Display>, tokio::task::JoinError>,
+) {
+    match outcome {
+        Ok(Ok(())) => {
+            info!("{} has exited", task_name);
+        }
+        Ok(Err(err)) => {
+            error!(
+                error.cause_chain = ?err,
+                error.message = %err,
+                "{} failed",
+                task_name,
+            )
+        }
+        Err(err) => {
+            error!(
+                error.cause_chain = ?err,
+                error.message = %err,
+                "'{}' task failed to complete",
+                task_name,
+            )
+        }
+    }
 }
